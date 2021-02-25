741aea164f6d5f8a3f798c04ff251d8dd2e1a3da
clarinha-prado <clarinha.prado@gmail.com>
2021-02-25 11:34:11 -0300

    final do dia 4

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://avatars.githubusercontent.com/u/54044330?s=400&v=4" alt="Clarinha Prado" />
@@ -8,7 +12,7 @@ export function Profile() {
                 <strong>Clarinha Prado</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
